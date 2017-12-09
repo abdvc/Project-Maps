a753257dac91ee04681822f234b472475131d978
Golemh <34375335+Golemh@users.noreply.github.com>
2017-12-09 19:19:30 +0500

    Create LinkGenerator.java

---
+++ LinkGenerator.java
@@ -0,0 +1,45 @@
+public class LinkGenerator {
+    StringBuilder Param = new StringBuilder();
+    String API_key = "";
+    
+        //set or change a specific parameter. i.e zoom, maptype.
+    //para = specific parameter to set. value: the value assigned to that parameter.
+    void setOverwriteParam(String para, String value) {  
+        para.replace(" ", "+");
+       if (Param.indexOf(para) == -1) {                     
+           para = "&" + para;
+            Param.append(para + "=" + value);
+        }
+       else {
+           int start = Param.indexOf(para);
+           int end = start + para.length() - 1;
+           Param.replace(start, end, para);
+           for (int i = end; i <= Param.length(); i++) {
+               if (Param.charAt(i) == '&') {
+                   Param.replace(end+1, i-1, "="+value);
+               }
+               else if (i == Param.length()) {
+                   Param.replace(end+1, i-1, "="+value);
+               }
+           }
+                   }
+    }
+    
+    
+    /*set parameters to add objects. such as markers, lines etc.
+      value and parameter name must be given in a single String literal.
+    */
+    void setParam(String para) {  
+        Param.append(para);
+    }
+    
+    String generateLink() {
+        String a = "https://maps.googleapis.com/maps/api/staticmap?";
+        a += Param + "&" + API_key;
+        return a;
+    }
+    
+    void setApiKey(String a) {
+        API_key = a;
+    }
+}
