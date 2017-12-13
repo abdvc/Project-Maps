a7bcd59a1ab45d2d2b8b019c1643af621943bb70
abdvc <34377465+abdvc@users.noreply.github.com>
2017-12-13 16:56:30 +0500

    Update graph.java

--- graph.java
+++ graph.java
@@ -3,241 +3,73 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication84;
+package javaapplication88;
 
-import java.util.Stack;
+import java.util.LinkedList;
 
 /**
  *
  * @author Abdullah
  */
-public class graph {
-
-    vertex head = null;
-    vertex tail;
-    Stack<String> s = new Stack<String>();
-    Queue que;
-
-    graph() {
-        que = new Queue(10);
+public class Graph {
+    LinkedList<Edge>[] list;
+    LinkedList<Vertex> vList;
+    
+    Graph(LinkedList<Vertex> list){
+        vList = list;
+        this.list = new LinkedList[list.size()];
+        for (int i = 0; i < this.list.length; i++) {
+            this.list[i] = new LinkedList<>();
+        }
     }
-
-    graph(int i) {
-        que = new Queue(i);
+    
+    public void insertEdge(String[] arr){
+        int h = Integer.parseInt(arr[0]);
+        Vertex v1 = findVert(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            Vertex v2 = findVert(arr[i]);
+            list[h-1].add(new Edge(v1, v2));
+        }
     }
-
-    public void BFS() {
-        vertex temp = head;
-
-        que.Enqueue(temp.label);
-        System.out.println("Calling BFS: ");
-        while (temp != null) {
-            vertex temp2 = temp.ver.head;
-            while (temp2 != null) {
-                if (!temp2.visited) {
-                    temp2.visited = true;
-                    que.Enqueue(temp2.label);
-                }
-                temp2 = temp2.Right;
-
+    
+    public Vertex findVert(String str){
+        for (int i = 0; i < vList.size(); i++) {
+            if (str.equals(vList.get(i).label)) {
+                return vList.get(i);
             }
-            que.DeQueue();
-            temp = temp.down;
-        }
-        while (!que.isEmpty()) {
-            que.DeQueue();
-        }
-    }
-
-    public void DFS() {
-        vertex temp = head;
-        s.push(head.label);
-        System.out.println("Calling DFS: ");
-        while (true) {
-            vertex finding = this.findUnvisited(temp);
-            if (finding != null) {
-                finding.visited = true;
-                s.push(finding.label);
-                temp = finding;
-            } else {
-
-                System.out.println(s.pop() + " ");
-                if (s.isEmpty()) {
-                    break;
-                }
-                temp = this.find(s.peek());
-
-            }
-        }
-    }
-
-    public vertex findUnvisited(vertex ver) {
-        vertex temp = ver.ver.head;
-        if (temp == null) {
-            return null;
-        }
-        while (temp.visited != false) {
-            temp = temp.Right;
-            if (temp == null) {
-                return null;
-            }
-        }
-        return temp;
-    }
-
-    public void insertVertex(String d) {
-        vertex v = new vertex(d);
-        if (head == null) {
-            head = v;
-            tail = head;
-        } else {
-            tail.down = v;
-            tail = tail.down;
-        }
-    }
-
-    public vertex find(String d) {
-        vertex temp = head;
-        while (temp != null) {
-            if (temp.label.equalsIgnoreCase(d)) {
-                return temp;
-            }
-            temp = temp.down;
         }
         return null;
     }
-
-    public void insertEdge(String vertex, String edge) {
-        vertex temp = find(vertex);
-        vertex temp2 = find(edge);
-        if (temp != null) {
-            temp.ver.insert(temp2);
-        }
-    }
-
-    public void Display() {
-        vertex temp = head;
-        while (temp != null) {
-            System.out.println(temp.toString());
-            temp = temp.down;
-        }
-    }
-
-}
-
-class LinkedList {
-
-    vertex head;
-    vertex tail;
-
-    public void insert(vertex v) {
-
-        if (head == null) {
-            head = v;
-            tail = head;
-        } else {
-            tail.Right = v;
-            tail = tail.Right;
-        }
-
-    }
-
-    public String toString() {
-        vertex temp = head;
-        String str = "Is connected to: ";
-        while (temp != null) {
-            str = str + temp.label + " ";
-            temp = temp.Right;
-        }
-        return str;
-
-    }
-
-}
-
-class vertex {
-
-    String label;
-    vertex Right;
-    vertex down;
-    LinkedList ver = new LinkedList();
-    boolean visited = false;
-
-    vertex(String d) {
-        label = d;
-    }
-
-    public String toString() {
-        String str = "";
-        str = str + this.label + " " + ver.toString();
-        return str;
-    }
-
-}
-
-class Queue {
-
-    int Front;
-    int Rear;
-    String arr[];
-
-    Queue() {
-        Front = 0;
-        Rear = 0;
-        arr = new String[10];
-    }
-
-    Queue(int size) {
-        Front = 0;
-        Rear = 0;
-        arr = new String[size];
-    }
-
-    public boolean isEmpty() {
-        return Rear == Front;
-    }
-
-    public boolean isFull() {
-        if (Rear - Front == arr.length || Front - Rear == 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public void DeQueue() {
-        if (Front == arr.length) {
-            Front = 0;
-        }
-        System.out.println(arr[Front]);
-        arr[Front] = "";
-        Front = Front + 1;
-
-    }
-
-    public void Dispplay() {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
-    }
-
-    public void Enqueue(String data) {
-        if (!isFull()) {
-            if (Rear != arr.length) {
-                arr[Rear] = data;
-                //System.out.println("Data: "+data+" arra posi : "+Rear);
-                Rear = Rear + 1;
-
-            } else {
-                Rear = 0;
-                arr[Rear] = data;
-                //System.out.println("Data: "+data+" arra posi : "+Rear);
-                Rear = Rear + 1;
-
+    
+    public void display(){
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list[i].size(); j++) {
+                System.out.println(list[i].get(j).weight);
             }
-
-        } else {
-            System.out.println("Not possible");
         }
+    }
+}
+public class Vertex {
+    String label;
+    double lat;
+    double lon;
+    
+    Vertex(String str){
+        String[] s1 = str.split(" ");
+        label = s1[0];
+        String[] s2 = s1[1].split(",");
+        lat = Double.parseDouble(s2[0]);
+        lon = Double.parseDouble(s2[1]);
+    }
+}
+public class Edge {
+
+    double weight;
+    Vertex src;
+    Vertex dst;
+
+    Edge(Vertex v1, Vertex v2) {
+        weight = Math.sqrt(Math.pow(v2.lat - v1.lat, 2)
+                + Math.pow(v2.lon - v1.lon, 2));
     }
 }
